e164cb84078472329f49b11b78391c94e0f68e0b
Martin Nedev <martin.nedev@icloud.com>
2026-02-20 13:14:55 +0000

    feat: add directives parsing

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 # nginx-dav-module (skeleton)
 
+
+## Configuration Directives
+
+# dav_create_full_path
+Syntax: 	dav_create_full_path on | off;
+Default:  dav_create_full_path off;
+Context: 	http, server, location
+
+This directive allows creating all needed intermediate directories. 
+
+# dav_access
+Syntax: 	dav_access users:permissions ...;
+Default: 	dav_access user:rw;
+Context: 	http, server, location
+
+Sets access permissions for newly created files and directories.
+
+# dav_delete_depth
+Syntax: 	dav_delete_depth number;
+Default: 	dav_delete_depth 0;
+Context: 	http, server, location
+
+Allows the DELETE method to remove files provided that the number of elements in a request path is not less than the specified number.
+
+# dav_methods
+Syntax: 	dav_methods off | method ...;
+Default: 	dav_methods off;
+Context: 	http, server, location
+
+Allows the specified HTTP and WebDAV methods. The parameter off denies all methods processed by this module.
+
+##
+
 This directory contains a minimal skeleton for an Nginx HTTP module that can
 be built into the nginx binary in your workspace (no system install required).
 
--- ngx_http_dav_module.c
+++ ngx_http_dav_module.c
@@ -5,24 +5,66 @@
 static ngx_int_t ngx_http_dav_handler(ngx_http_request_t *r);
 static ngx_int_t ngx_http_dav_init(ngx_conf_t *cf);
 
+typedef struct {
+        ngx_flag_t    create_full_path;
+        ngx_array_t  *dav_methods;
+        ngx_array_t  *dav_access;
+        ngx_uint_t    min_delete_depth;
+} ngx_http_dav_loc_conf_t;
+
+static void *ngx_http_dav_create_loc_conf(ngx_conf_t *cf);
+static char *ngx_http_dav_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
+
+static ngx_command_t ngx_http_dav_commands[] = {
+        { ngx_string("dav_create_full_path"),
+            NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
+            ngx_conf_set_flag_slot,
+            NGX_HTTP_LOC_CONF_OFFSET,
+            offsetof(ngx_http_dav_loc_conf_t, create_full_path),
+            NULL },
+
+        { ngx_string("dav_methods"),
+            NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_1MORE,
+            ngx_conf_set_str_array_slot,
+            NGX_HTTP_LOC_CONF_OFFSET,
+            offsetof(ngx_http_dav_loc_conf_t, dav_methods),
+            NULL },
+
+        { ngx_string("dav_access"),
+            NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_1MORE,
+            ngx_conf_set_str_array_slot,
+            NGX_HTTP_LOC_CONF_OFFSET,
+            offsetof(ngx_http_dav_loc_conf_t, dav_access),
+            NULL },
+
+        { ngx_string("dav_delete_depth"),
+            NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+            ngx_conf_set_num_slot,
+            NGX_HTTP_LOC_CONF_OFFSET,
+            offsetof(ngx_http_dav_loc_conf_t, min_delete_depth),
+            NULL },
+
+        ngx_null_command
+};
+
 static ngx_http_module_t ngx_http_dav_module_ctx = {
-    NULL,
-    ngx_http_dav_init,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL
+        NULL,
+        ngx_http_dav_init,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        ngx_http_dav_create_loc_conf,
+        ngx_http_dav_merge_loc_conf
 };
 
 ngx_module_t ngx_http_dav_module = {
-    NGX_MODULE_V1,
-    &ngx_http_dav_module_ctx,
-    NULL,
-    NGX_HTTP_MODULE,
-    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-    NGX_MODULE_V1_PADDING
+        NGX_MODULE_V1,
+        &ngx_http_dav_module_ctx,
+        ngx_http_dav_commands,
+        NGX_HTTP_MODULE,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NGX_MODULE_V1_PADDING
 };
 
 static ngx_int_t
@@ -50,4 +92,50 @@ ngx_http_dav_init(ngx_conf_t *cf)
     *h = ngx_http_dav_handler;
 
     return NGX_OK;
+}
+
+static void *
+ngx_http_dav_create_loc_conf(ngx_conf_t *cf)
+{
+    ngx_http_dav_loc_conf_t  *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_dav_loc_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+
+    conf->create_full_path = NGX_CONF_UNSET;
+    conf->dav_methods = NGX_CONF_UNSET_PTR;
+    conf->dav_access = NGX_CONF_UNSET_PTR;
+    conf->min_delete_depth = NGX_CONF_UNSET_UINT;
+
+    return conf;
+}
+
+static char *
+ngx_http_dav_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+{
+    ngx_http_dav_loc_conf_t *prev = parent;
+    ngx_http_dav_loc_conf_t *conf = child;
+
+    ngx_conf_merge_value(conf->create_full_path, prev->create_full_path, 0);
+    ngx_conf_merge_uint_value(conf->min_delete_depth, prev->min_delete_depth, 0);
+
+    if (conf->dav_methods == NGX_CONF_UNSET_PTR) {
+        if (prev->dav_methods != NGX_CONF_UNSET_PTR) {
+            conf->dav_methods = prev->dav_methods;
+        } else {
+            conf->dav_methods = NULL;
+        }
+    }
+
+    if (conf->dav_access == NGX_CONF_UNSET_PTR) {
+        if (prev->dav_access != NGX_CONF_UNSET_PTR) {
+            conf->dav_access = prev->dav_access;
+        } else {
+            conf->dav_access = NULL;
+        }
+    }
+
+    return NGX_CONF_OK;
 }
